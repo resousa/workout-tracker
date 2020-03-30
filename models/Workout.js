62b26fabbff1ce8c54200644cd97e3ad1b9e36ad
Ryan Sousa <Sousa.Ryan4@gmail.com>
2020-03-30 03:53:07 -0700

    fix chart duration issue

--- models/Workout.js
+++ models/Workout.js
@@ -1,46 +1,57 @@
-'use strict';
+"use strict";
 
 const mongoose = require(`mongoose`);
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema(
-    {
-        day: {
-            type: Date,
-            default: Date.now,
-          },
-          exercises: [
-            {
-              type: {
-                type: String,
-                trim: true,
-                required: "enter type"
-              },
-              name: {
-                type: String,
-                trim: true,
-                required: "enter name"
-              },
-              weight: {
-                type: Number
-              },
-              sets: {
-                type: Number
-              },
-              reps: {
-                type: Number
-              },
-              duration: {
-                type: Number,
-                required: "enter duration"
-              },
-              distance: {
-                type: Number
-              }
-            }
-          ]
+const Workout = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "enter type"
         },
-      );
+        name: {
+          type: String,
+          trim: true,
+          required: "enter name"
+        },
+        weight: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        duration: {
+          type: Number,
+          required: "enter duration"
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+);
 
-module.exports = mongoose.model(`Workout`, WorkoutSchema);
+Workout.virtual("totalDuration").get(function() {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+module.exports = mongoose.model(`Workout`, Workout);
