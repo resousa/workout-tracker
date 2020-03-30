9063b1986997d7dce761e5e02e2611ea7f3ebaf2
Ryan Sousa <Sousa.Ryan4@gmail.com>
2020-03-30 03:30:19 -0700

    routes added

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,4 +3,46 @@
 const db = require(`../models`);
 
 module.exports = app => {
+  app.get(`/api/workouts`, (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+  app.get(`/api/workouts/range`, (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+  app.post(`/api/workouts`, (req, res) => {
+    db.Workout.create({})
+      .then(newWorkout => {
+        res.json(newWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+  app.put(`/api/workouts/:id`, (req, res) => {
+    db.Workout.update(
+      { _id: req.params.id },
+      { $push: { exercises: req.body } }
+    )
+      .then(updatedWorkout => {
+        res.json(updatedWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
 };
